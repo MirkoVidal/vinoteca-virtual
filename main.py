faa1205551e7bb078ea2f37780801b11fe8ca6c0
Mirko <mirkovidal2023@gmail.com>
2025-02-06 21:40:36 -0300

    Corregido el archivo main.py para Gunicorn

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Flask
 from flask import Flask
 from flask_restful import Api
 
@@ -7,17 +6,18 @@ from vinoteca import Vinoteca
 # API
 from recursos import *
 
+# Inicializa la aplicación Flask
+app = Flask(__name__)
+
+# Configura la API
+api = Api(app)
+api.add_resource(RecursoBodega, '/api/bodegas/<id>')
+api.add_resource(RecursoBodegas, '/api/bodegas')
+api.add_resource(RecursoCepa, '/api/cepas/<id>')
+api.add_resource(RecursoCepas, '/api/cepas')
+api.add_resource(RecursoVino, '/api/vinos/<id>')
+api.add_resource(RecursoVinos, '/api/vinos')
+
 if __name__ == "__main__":
     Vinoteca.inicializar()
-
-    app = Flask(__name__)
-
-    api = Api(app)
-    api.add_resource(RecursoBodega, '/api/bodegas/<id>')
-    api.add_resource(RecursoBodegas, '/api/bodegas')
-    api.add_resource(RecursoCepa, '/api/cepas/<id>')
-    api.add_resource(RecursoCepas, '/api/cepas')
-    api.add_resource(RecursoVino, '/api/vinos/<id>')
-    api.add_resource(RecursoVinos, '/api/vinos')
-
     app.run(debug=False)
